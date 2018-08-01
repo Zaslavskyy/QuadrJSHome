ad7325e697951011a2e05f641d5dec240fe927bd
Oleh Zaslavskyy <oleh.zaslavskyy@gmail.com>
2018-08-01 13:31:20 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ const b = parseFloat(prompt('Enter value of b'));
 const c = parseFloat(prompt('Enter value of c'));
 
 function calcD(a, b, c) {
-const result = b * b - 4* a * c; 
-return result;
+ const result = b * b - 4 * a * c;
+ return result;
 }
 
 const d = calcD(a, b, c);
 alert("D=" + d);
 
 function solveQuadr1(a, b, c) {
-const D = b * b - 4* a * c;
-if (D == 0) {
-const result = -b / 2 * a;
-return result;
+ const D = b * b - 4 * a * c;
+ if (D == 0) {
+  const result = -b / (2 * a);
+  return result;
  }
 }
 
@@ -22,10 +22,10 @@ const X = solveQuadr1(a, b, c);
 alert("x=" + X);
 
 function solveQuadr2(a, b, c) {
-const D = b * b - 4* a * c;
-if (D > 0) {
-const result = (-b + (Math.sqrt(d)) / 2 * a);
-return result;
+ const D = b * b - 4 * a * c;
+ if (D > 0) {
+  const result = (-b + (Math.sqrt(d)) / (2 * a));
+  return result;
  }
 }
 
@@ -33,21 +33,19 @@ const x = solveQuadr2(a, b, c);
 alert("x1=" + x);
 
 function solveQuadr3(a, b, c) {
-const D = b * b - 4* a * c;
-if (D > 0) {
-const result = (-b - (Math.sqrt(d)) / 2 * a);
-return result;
-}
+ const D = b * b - 4 * a * c;
+ if (D > 0) {
+  const result = (-b - (Math.sqrt(d)) / (2 * a));
+  return result;
  }
+}
 
 const x2 = solveQuadr3(a, b, c);
 alert("x2=" + x2);
 
 function solveQuadr4(a, b, c) {
-    const D = b * b - 4* a * c;
-if (D < 0) {
-    alert("No results");
+ const D = b * b - 4 * a * c;
+ if (D < 0) {
+  alert("No results");
+ }
 }
-}
-
-
