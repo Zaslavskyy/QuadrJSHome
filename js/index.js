e619fdcf589ce214626b7e0a5a4b418ec71d99b9
Oleh Zaslavskyy <oleh.zaslavskyy@gmail.com>
2018-08-02 08:44:10 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -3,28 +3,16 @@ const b = parseFloat(prompt('Enter value of b'));
 const c = parseFloat(prompt('Enter value of c'));
 
 function calcD(a, b, c) {
- const result = b * b - 4 * a * c;
+ const result = b*b - 4*a*c;
  return result;
 }
 
 const d = calcD(a, b, c);
 alert("D=" + d);
 
-function solveQuadr1(a, b, c) {
- const D = b * b - 4 * a * c;
- if (D == 0) {
-  const result = -b / (2 * a);
-  return result;
- }
-}
-
-const X = solveQuadr1(a, b, c);
-alert("x=" + X);
-
 function solveQuadr2(a, b, c) {
- const D = b * b - 4 * a * c;
- if (D > 0) {
-  const result = (-b + (Math.sqrt(d)) / (2 * a));
+ if (d > 0) {
+  const result = (-b + Math.sqrt(d))/(2*a);
   return result;
  }
 }
@@ -33,9 +21,9 @@ const x = solveQuadr2(a, b, c);
 alert("x1=" + x);
 
 function solveQuadr3(a, b, c) {
- const D = b * b - 4 * a * c;
- if (D > 0) {
-  const result = (-b - (Math.sqrt(d)) / (2 * a));
+
+ if (d > 0) {
+  const result = (-b - Math.sqrt(d))/(2*a);
   return result;
  }
 }
@@ -44,8 +32,18 @@ const x2 = solveQuadr3(a, b, c);
 alert("x2=" + x2);
 
 function solveQuadr4(a, b, c) {
- const D = b * b - 4 * a * c;
- if (D < 0) {
+ if (d < 0) {
   alert("No results");
  }
 }
+
+function solveQuadr1(a, b, c) {
+
+ if (d == 0) {
+  const result = -b/(2*a);
+  return result;
+ }
+}
+
+const X = solveQuadr1(a, b, c);
+alert("x=" + X);
